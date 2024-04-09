4b0a7c71070a36bbb07f98ddd7c9f72e630bab6d
Dmn <56503938+DmnCdn@users.noreply.github.com>
2024-04-09 09:40:10 +0200

    Project setup
    * Created a project in Android Studio
    * Added a custom color palette
    * Slightly updated readme

---
+++ app/src/main/java/com/example/foodreminder/ui/theme/Color.kt
@@ -0,0 +1,61 @@
+package com.example.foodreminder.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// LIGHT MODE COLORS
+val colorPrimaryLight = Color(0xff006b5b)
+val colorOnPrimaryLight = Color(0xffffffff)
+val colorPrimaryContainerLight = Color(0xff78f8dd)
+val colorOnPrimaryContainerLight = Color(0xff00201a)
+
+val colorSecondaryLight = Color(0xff4a635d)
+val colorOnSecondaryLight = Color(0xffffffff)
+val colorSecondaryContainerLight = Color(0xffcde8e0)
+val colorOnSecondaryContainerLight = Color(0xff06201b)
+
+val colorTertiaryLight = Color(0xff436278)
+val colorOnTertiaryLight = Color(0xffffffff)
+val colorTertiaryContainerLight = Color(0xffc8e6ff)
+val colorOnTertiaryContainerLight = Color(0xff001e2f)
+
+val colorErrorLight = Color(0xffba1a1a)
+val colorOnErrorLight = Color(0xffffffff)
+val colorErrorContainerLight = Color(0xffffdad6)
+val colorOnErrorContainerLight = Color(0xff410002)
+
+val colorBackgroundLight = Color(0xfffafdfa)
+val colorOnBackgroundLight = Color(0xff191c1b)
+val colorSurfaceLight = Color(0xfffafdfa)
+val colorOnSurfaceLight = Color(0xff191c1b)
+val colorOutlineLight = Color(0xff6f7976)
+val colorSurfaceVariantLight = Color(0xffdbe5e0)
+val colorOnSurfaceVariantLight = Color(0xff3f4946)
+
+// DARK MODE COLORS
+val colorPrimaryDark = Color(0xff59dbc1)
+val colorOnPrimaryDark = Color(0xff00382f)
+val colorPrimaryContainerDark = Color(0xff005144)
+val colorOnPrimaryContainerDark = Color(0xff78f8dd)
+
+val colorSecondaryDark = Color(0xffb1ccc4)
+val colorOnSecondaryDark = Color(0xff1d352f)
+val colorSecondaryContainerDark = Color(0xff334b45)
+val colorOnSecondaryContainerDark = Color(0xffcde8e0)
+
+val colorTertiaryDark = Color(0xffabcae4)
+val colorOnTertiaryDark = Color(0xff113348)
+val colorTertiaryContainerDark = Color(0xff2b4a5f)
+val colorOnTertiaryContainerDark = Color(0xffc8e6ff)
+
+val colorErrorDark = Color(0xffffb4ab)
+val colorOnErrorDark = Color(0xff690005)
+val colorErrorContainerDark = Color(0xff93000a)
+val colorOnErrorContainerDark = Color(0xffffdad6)
+
+val colorBackgroundDark = Color(0xff191c1b)
+val colorOnBackgroundDark = Color(0xffe0e3e1)
+val colorSurfaceDark = Color(0xff191c1b)
+val colorOnSurfaceDark = Color(0xffe0e3e1)
+val colorOutlineDark = Color(0xff89938f)
+val colorSurfaceVariantDark = Color(0xff3f4946)
+val colorOnSurfaceVariantDark = Color(0xffbec9c5)
