706bf45f3bc54241a43bab1dcb46eea83995276c
DmnCdn <damian.latka@o2.pl>
2024-04-09 15:12:31 +0200

    Start of android application plugin

---
+++ buildlogic/convention/src/main/java/com/example/foodreminder/convention/plugins/AndroidApplicationPlugin.kt
@@ -0,0 +1,36 @@
+package com.example.foodreminder.convention.plugins
+
+import com.android.build.gradle.AppExtension
+import com.example.foodreminder.convention.utils.getVersionByName
+import com.example.foodreminder.convention.utils.libs
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.getByType
+
+class AndroidApplicationPlugin: Plugin<Project> {
+    override fun apply(project: Project) {
+        with(project) {
+            with(pluginManager) {
+                apply("com.android.application")
+                apply("org.jetbrains.kotlin.android")
+            }
+
+            with(extensions.getByType<AppExtension>()) {
+                compileSdkVersion = libs.getVersionByName("compileSdk")
+
+                with(defaultConfig) {
+                    applicationId = "com.example.foodreminder"
+                    minSdk = libs.getVersionByName("minSdk").toInt()
+                    targetSdk = libs.getVersionByName("targetSdk").toInt()
+                    versionCode = libs.getVersionByName("versionCode").toInt()
+                    versionName = libs.getVersionByName("versionName")
+
+                    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+                    vectorDrawables {
+                        useSupportLibrary = true
+                    }
+                }
+            }
+        }
+    }
+}
