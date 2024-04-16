c6a0cfca77988f8cd005acb1b9661f16eafa6fa5
DmnCdn <damian.latka@o2.pl>
2024-04-16 10:13:03 +0200

    Created a compose gradle plugin

---
+++ buildlogic/convention/src/main/java/com/example/foodreminder/convention/utils/DependencyHandlerExtensions.kt
@@ -0,0 +1,56 @@
+package com.example.foodreminder.convention.utils
+
+import org.gradle.api.artifacts.Dependency
+import org.gradle.api.artifacts.ExternalModuleDependencyBundle
+import org.gradle.api.artifacts.MinimalExternalModuleDependency
+import org.gradle.api.artifacts.dsl.DependencyHandler
+import org.gradle.api.provider.Provider
+import org.gradle.kotlin.dsl.DependencyHandlerScope
+import org.gradle.kotlin.dsl.add
+
+/**
+ * An extension that allows implementing a dependency in the Kotlin DSL `dependency {}` block.
+ * It is equivalent to `implementation(libs.dependency.name)` in `build.gradle.kts`.
+ *
+ * @param dependency notation for the dependency to be added.
+ * @return The dependency.
+ *
+ * @see [DependencyHandler.add]
+ */
+internal fun DependencyHandlerScope.implementation(dependency: Provider<MinimalExternalModuleDependency>): Dependency? =
+    add("implementation", dependency)
+
+/**
+ * An extension that allows implementing a dependency in the Kotlin DSL `dependency {}` block.
+ * It is equivalent to `implementation(libs.dependency.name)` in `build.gradle.kts`.
+ *
+ * @param dependency notation for the dependency to be added.
+ * @return The dependency.
+ *
+ * @see [DependencyHandler.add]
+ */
+internal fun DependencyHandlerScope.debugImplementation(dependency: Provider<MinimalExternalModuleDependency>): Dependency? =
+    add("debugImplementation", dependency)
+
+/**
+ * An extension that allows implementing a bundle in the Kotlin DSL `dependency {}` block.
+ * It is equivalent to `implementation(libs.bundles.name)` in `build.gradle.kts`.
+ *
+ * @param dependency Notation for the dependency to be added.
+ * @return The dependency.
+ *
+ * @see [DependencyHandler.add]
+ */
+internal fun DependencyHandlerScope.bundleImplementation(dependency: Provider<ExternalModuleDependencyBundle>): Dependency? =
+    add("implementation", dependency)
+
+/**
+ * Adds a dependency to the 'testImplementation' configuration.
+ *
+ * @param dependencyNotation notation for the dependency to be added.
+ * @return The dependency.
+ *
+ * @see [DependencyHandler.add]
+ */
+internal fun DependencyHandlerScope.testImplementation(dependencyNotation: Any): Dependency? =
+    add("testImplementation", dependencyNotation)
